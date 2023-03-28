df10b9a19c3ffb574d9613f86b4e013fb17ef035
gandhisetyawan <gandhisetyawan1.gs@gmail.com>
2023-03-28 16:38:12 +0700

    app learn module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gandhisetyawan/app-learn-module
+
+go 1.19
+
+require github.com/gandhisetyawan/go-module-learn v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/gandhisetyawan/go-module-learn"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello)
+}
