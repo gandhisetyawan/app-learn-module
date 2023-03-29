45818eb003bc4e117aff18033b6706af2cd3fccd
gandhisetyawan <gandhisetyawan1.gs@gmail.com>
2023-03-29 10:00:03 +0700

    update module use v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/gandhisetyawan/app-learn-module
 
 go 1.19
 
-require github.com/gandhisetyawan/go-module-learn v1.0.0 // indirect
+require github.com/gandhisetyawan/go-module-learn/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 
 import (
 	"fmt"
-	go_say_hello "github.com/gandhisetyawan/go-module-learn"
+	go_say_hello "github.com/gandhisetyawan/go-module-learn/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("gandhi"))
 }
